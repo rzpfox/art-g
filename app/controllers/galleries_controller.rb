584b0fad6ce6c82ac82afc9f5f5c347eb5117f69
deng gong <rzpfox@gmail.com>
2018-11-28 17:03:49 +0800

    built controllers

---
+++ app/controllers/galleries_controller.rb
@@ -0,0 +1,40 @@
+class GalleriesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @galleries = Gallery.all
+  end
+
+  def show
+    @gallery = Gallery.find(params[:id]
+  end
+
+  def new
+    @gallery = Gallery.new
+  end
+
+  def create
+    @gallery = Gallery.new(gallery_params)
+    @gallery.save
+  end
+
+  def edit
+    @gallery = Gallery.find(params[:id])
+  end
+
+  def update
+    @gallery = Gallery.find(params[:id])
+    @gallery.update(gallery_params)
+  end
+
+  def destroy
+    @gallery = Gallery.find(params[:id])
+    @gallery.destroy(gallery_params)
+  end
+
+  private
+
+  def gallery_params
+    params.require(:gallery).permit(:name, :address, :contact_name, :painting_id, :gallery_id)
+  end
+end
---
+++ app/controllers/paintings_controller.rb
@@ -0,0 +1,46 @@
+class PaintingsController < ApplicationController
+  def index
+    @paintings = Painting.all
+  end
+
+  def show
+    @gallery = Gallery.find(params[:gallery_id]
+    @painting = painting.find(params[:id]
+  end
+
+  def new
+    @gallery = Gallery.find(params[:gallery_id]
+    @painting = Painting.new
+
+  end
+
+  def create
+    @gallery = Gallery.find(params[:gallery_id]
+    @painting = Painting.new(painting_params)
+    @painting.gallery = @gallery
+    @painting.save
+  end
+
+  def edit
+    @gallery = Gallery.find(params[:gallery_id]
+    @painting = painting.find(params[:id]
+  end
+
+  def update
+    @gallery = Gallery.find(params[:gallery_id]
+    @painting = painting.find(params[:id]
+    @painting.update(painting_params)
+  end
+
+  def destroy
+    @gallery = Gallery.find(params[:gallery_id]
+    @painting = painting.find(params[:id]
+    @painting.destroy(painting_params)
+  end
+
+  private
+
+  def painting_params
+    params.require(:painting).permit(:title, :description, :artist_name, :status, :value, :gallery_id, :painting_id)
+  end
+end
