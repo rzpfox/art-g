184153a87e4159dec83bd44e337c2266dc1b128d
Jun Yuan <mediter@me.com>
2018-12-06 15:37:20 +0800

    initial appointment setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
+
   get '/paintings', to: 'paintings#index', as: 'all_my_paintings'
   get '/galleries/all', to: 'galleries#all', as: 'all_galleries'
   get '/paintings/all', to: 'paintings#all', as: 'all_paintings'
   get '/artists/all', to: 'artists#all', as: 'all_artists'
-  resources :galleries, :artists do
+
+  resources :galleries do
     resources :paintings, shallow: true, except: :index
+    resources :appointments, shallow: true
   end
 
-  resources :appointments
+  resources :artists
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
