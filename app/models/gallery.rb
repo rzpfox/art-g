4ad14297f75cbef12bcc8e426b7a47a17fb289e4
randy li <randy.mar.li@gmail.com>
2018-12-07 17:04:11 +0800

    geocode for gallery done

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -9,6 +9,7 @@ class Gallery < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
+
   include PgSearch
   multisearchable :against => [:name, :address]
 end
