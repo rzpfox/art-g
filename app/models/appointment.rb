3f2125110ccdef99abd004191deec2fa44c486e0
deng gong <rzpfox@gmail.com>
2018-12-11 00:21:03 +0800

    edited css html on gallery show page with minor changes on other pages.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,6 @@ class Appointment < ApplicationRecord
   belongs_to :gallery
 
   validates :visitor_name, :visitor_email, :start_time, :token, presence: true
+  include PgSearch
+  multisearchable :against => [:visitor_email]
 end
