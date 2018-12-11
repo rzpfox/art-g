0fcf8a1cccb4d67fa0e1448a9a87a5818cb9d39d
deng gong <rzpfox@gmail.com>
2018-12-11 15:20:11 +0800

    added config.assets.initialize_on_precompile = false to config/application.rb and config.assets.enabled = true t

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module ArtG
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
+    config.assets.enabled = true
+
 
 
     # Settings in config/environments/* take precedence over those specified here.
